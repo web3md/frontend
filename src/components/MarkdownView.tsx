79f56adc8536cefef3cfeed96b2c9f6243d5c206
Team LevX <team@levx.app>
2020-11-22 19:22:04 +0900

    Update markdown styles

--- src/components/MarkdownView.tsx
+++ src/components/MarkdownView.tsx
@@ -8,9 +8,9 @@ const MarkdownView = ({ text }) => {
     const { textDark, accent, backgroundLight } = useColors();
     const styles = {
         heading1: { fontFamily: "bold", marginTop: Spacing.normal, marginBottom: Spacing.tiny },
-        heading2: { fontFamily: "bold", marginTop: Spacing.small, marginBottom: Spacing.tiny },
+        heading2: { fontFamily: "bold", marginTop: Spacing.normal, marginBottom: Spacing.tiny },
         heading3: { fontFamily: "bold", marginTop: Spacing.small, marginBottom: Spacing.tiny },
-        heading4: { fontFamily: "bold", marginTop: Spacing.tiny },
+        heading4: { fontFamily: "bold", marginTop: Spacing.small },
         heading5: { fontFamily: "bold", marginTop: Spacing.tiny },
         heading6: { fontFamily: "bold", marginTop: Spacing.tiny },
         body: { fontFamily: "regular", color: textDark, fontSize: 15, lineHeight: 27.5 },
